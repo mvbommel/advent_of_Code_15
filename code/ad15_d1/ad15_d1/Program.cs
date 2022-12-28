69febdbc8c5fc69ccff4ba841e6c0bcc759298c1
marc van bommel <mijngamemail@ziggo.nl>
2022-12-28 16:36:58 +0100

    day 1

---
+++ code/ad15_d1/ad15_d1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+char[] file = File.ReadAllText(@"..\..\..\..\..\..\txt\day1.txt").ToCharArray();
+
+int position = 1;
+int floor = 0;
+foreach(char c in file)
+{
+    if(c== '(')
+    {
+        floor++;
+    }
+    else if( c ==')')
+    {
+        floor--;
+    }
+    if(floor <= -1)
+    {
+
+        break;
+    }
+    position++;
+}
+Console.WriteLine(position);
