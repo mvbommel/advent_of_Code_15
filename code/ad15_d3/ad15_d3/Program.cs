174742cf579ea4223d53559430cea2f74789546e
marc van bommel <mijngamemail@ziggo.nl>
2022-12-29 20:26:29 +0100

    day 3 part 2

--- code/ad15_d3/ad15_d3/Program.cs
+++ code/ad15_d3/ad15_d3/Program.cs
@@ -4,45 +4,88 @@ char[] file = File.ReadAllText(@"..\..\..\..\..\..\txt\day3.txt").ToCharArray();
 Console.WriteLine(file.Length);
 List<house> houses = new List<house>();
 houses.Add(new house() { x = 0, y = 0 });
-int x = 0;
-int y = 0;
-foreach(char c in file)
+int santaX = 0;
+int santaY = 0;
+int robotX = 0;
+int robotY = 0;
+for (int i = 0; i < file.Length; i++)
 {
     house house = new house();
-    switch (c)
+    if (i % 2 == 0)
     {
-        case '^':
-            y++;
-            house.x = x;
-            house.y = y;
-            break;
-        case 'v':
-            y--;
-            house.x = x;
-            house.y = y;
-            break;
-        case '>':
-            x++;
-            house.x = x;
-            house.y = y;
-            break;
-        case '<':
-            x--;
-            house.x = x;
-            house.y = y;
-            break;
-    }
-    bool contained = false;
-    foreach (house h in houses)
-    {
-        if (h.x == house.x && h.y == house.y)
+        switch (file[i])
         {
-            contained = true;
+            case '^':
+                robotY++;
+                house.x = robotX;
+                house.y = robotY;
+                break;
+            case 'v':
+                robotY--;
+                house.x = robotX;
+                house.y = robotY;
+                break;
+            case '>':
+                robotX++;
+                house.x = robotX;
+                house.y = robotY;
+                break;
+            case '<':
+                robotX--;
+                house.x = robotX;
+                house.y = robotY;
+                break;
+        }
+        bool contained = false;
+        foreach (house h in houses)
+        {
+            if (h.x == house.x && h.y == house.y)
+            {
+                contained = true;
+            }
+        }
+        if (!contained)
+        {
+            houses.Add(house);
         }
     }
-    if (!contained)
+    else
     {
-        houses.Add(house);
+        switch (file[i])
+        {
+            case '^':
+                santaY++;
+                house.x = santaX;
+                house.y = santaY;
+                break;
+            case 'v':
+                santaY--;
+                house.x = santaX;
+                house.y = santaY;
+                break;
+            case '>':
+                santaX++;
+                house.x = santaX;
+                house.y = santaY;
+                break;
+            case '<':
+                santaX--;
+                house.x = santaX;
+                house.y = santaY;
+                break;
+        }
+        bool contained = false;
+        foreach (house h in houses)
+        {
+            if (h.x == house.x && h.y == house.y)
+            {
+                contained = true;
+            }
+        }
+        if (!contained)
+        {
+            houses.Add(house);
+        }
     }
 }
 
