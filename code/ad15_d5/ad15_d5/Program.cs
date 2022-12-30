a7ab87394989e40001b5a4674575ee3579e2e16a
marc van bommel <mijngamemail@ziggo.nl>
2022-12-30 11:44:25 +0100

    day 5 part 1

---
+++ code/ad15_d5/ad15_d5/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] file = File.ReadAllLines(@"..\..\..\..\..\..\txt\day5.txt");
+int nice = 0;
+foreach(string line in file)
+{
+    int vowels = 0;
+    bool duplicate = false;
+    bool naughty = false;
+    char lastChar = '\0';
+    char[] chars = line.ToCharArray();
+    Console.WriteLine("chars " + chars.Length);
+    foreach (char c in chars)
+    {
+        if(c == 'a' || c == 'e'|| c == 'i' || c == 'o' || c == 'u') vowels++;
+        if(lastChar != '\0')
+        {
+            if(c == lastChar)
+            {
+                duplicate= true;
+            }
+
+            char[] two = { lastChar, c };
+            string s = new string(two);
+            if( s == "ab" || s == "cd" || s == "pq" || s == "xy")
+            {
+                naughty= true;
+            }
+        }
+        lastChar= c;
+    }
+    if(vowels >= 3 && duplicate && !naughty)
+    {
+        nice++;
+    }
+}
+Console.WriteLine(nice);
