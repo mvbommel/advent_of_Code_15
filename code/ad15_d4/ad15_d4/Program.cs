5d76c7683040fc1d9e09013c0c9e05d1301c748c
marc van bommel <mijngamemail@ziggo.nl>
2022-12-30 10:39:58 +0100

    day4

---
+++ code/ad15_d4/ad15_d4/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+string input = "ckczppom";
+bool found = false;
+int i = 0;
+while (!found)
+{
+    string temp = input + i;
+    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(temp);
+    byte[] hashBytes = MD5.Create().ComputeHash(inputBytes);
+    string hex = Convert.ToHexString(hashBytes);
+    char[] chars= hex.ToCharArray();
+    bool leadingZeros = true;
+    for(int j = 0; j <=5; j++)
+    {
+        char c = chars[j];
+        if(c != '0')
+        {
+            leadingZeros= false;
+        }
+    }
+    if(leadingZeros)
+    {
+        Console.WriteLine(hex);
+        found= true;
+        Console.WriteLine(i);
+    }
+    else
+    {
+        i++;
+    }
+}
