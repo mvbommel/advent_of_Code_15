ebfedc890868058982f25fd468891f3936ef3fca
marc van bommel <mijngamemail@ziggo.nl>
2023-01-13 11:51:07 +0100

    slow attempt at 11

---
+++ code/ad15_d11/ad15_d11/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = "hxbxwxba";
+
+string updatePassword(string input)
+{
+    char last = input.Last();
+    if (last != 'z')
+    {
+        if (last != 'h' && last != 'n' && last != 'k')
+        {
+            char next = (char)((int)last + 1);
+            input = input.Remove(input.Length - 1, 1) + (next);
+        }
+        else
+        {
+            char next = (char)((int)last + 2);
+            input = input.Remove(input.Length - 1, 1) + (next);
+        }
+    }
+    else
+    {
+        input = input.Remove(input.Length - 1, 1);
+        input = updatePassword(input);
+        input += 'a';
+    }
+    return input;
+}
+
+bool straight = false;
+bool NoForbidden = true;
+bool acceptable = false;
+int pairs = 0;
+while(!acceptable)
+{
+    input = updatePassword(input);
+    if(input.Contains("i") || input.Contains("o") || input.Contains("l")){
+        NoForbidden= false;
+    }
+    for(int i = 2; i <input.Length; i++)
+    {
+        if (input[i] - input[i - 1] == 1 && input[i] - input[i-2] == 2) { 
+            straight= true;
+        }
+    }
+    for (int i =12; i < input.Length; i++)
+    {
+        if (input[i] == input[i-1])
+        {
+            pairs++;
+        }
+    }
+
+    if (straight && NoForbidden && pairs >= 2)
+    {
+        acceptable= true;
+    }
+    Console.WriteLine(input);
+}
+Console.WriteLine(input);
