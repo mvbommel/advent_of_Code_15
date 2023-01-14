a3dd2399bc1e9a2f5a01430dc655dc8c26e86642
marc van bommel <mijngamemail@ziggo.nl>
2023-01-14 13:26:09 +0100

    day 12 part 1
    not really how they meant it but it works

---
+++ code/ad15_d12/ad15_d12/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string input = File.ReadAllText(@"..\..\..\..\..\..\txt\day12.txt");
+
+string[] parts = input.Split(new string[] { "[", "{", "]", "}", ":", ","}, StringSplitOptions.RemoveEmptyEntries);
+long total = 0;
+foreach(string part in parts)
+{
+    if (int.TryParse(part, out _))
+    {
+        Console.WriteLine(part);
+        total += int.Parse(part);
+    }
+}
+Console.WriteLine(total);
