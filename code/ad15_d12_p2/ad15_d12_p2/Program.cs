732ba4de46cf10f185bfe1d7b037281585ea8e52
marc van bommel <mijngamemail@ziggo.nl>
2023-01-15 12:03:16 +0100

    attempt at day 12 part 2

---
+++ code/ad15_d12_p2/ad15_d12_p2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+string json = File.ReadAllText(@"..\..\..\..\..\..\txt\day12.txt");
+
+dynamic obj = JsonConvert.DeserializeObject(json);
+
+
+
